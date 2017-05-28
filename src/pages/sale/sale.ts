8075fefae8717cd70fb8b0cc7eb2c1cf4d8cc556
Oliver Henrique <it.oliverhm@gmail.com>
2017-05-28 17:43:47 -0300

    Adiciona página de Sale.

---
+++ src/pages/sale/sale.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the Sale page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-sale',
+  templateUrl: 'sale.html'
+})
+export class SalePage {
+	endpoint: string = 'https://1-dot-full-market.appspot.com/promocao';
+	products: Array<any>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+		this.pullSales();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SalePage');
+  }
+
+  pullSales() {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.endpoint).map(res => {
+        try {
+          let json = res.json();
+          
+          return json;
+        } catch(e) {
+          console.log('error: ', e);
+        }
+      }).subscribe(data => {
+      	this.products = data;
+      });
+    });
+  }
+}
